4dd27e0b3a04e84029211e21da1df7f564f1ad06
flame78 <flame78@gmail.com>
2014-07-19 13:30:12 +0300

    Add figure and cell

---
+++ KingSurvivalRefactored/GameCore/Cell.cs
@@ -0,0 +1,12 @@
+﻿namespace KingSurvivalRefactored.GameCore
+{
+    public class Cell
+    {
+        public Cell(Figure figure)
+        {
+            this.ChessFigure = figure;
+        }
+
+        public Figure ChessFigure { get; set; }
+    }
+}
---
+++ KingSurvivalRefactored/GameCore/Figure.cs
@@ -0,0 +1,33 @@
+﻿namespace KingSurvivalRefactored.GameCore
+{
+    using System;
+
+    public class Figure
+    {
+        private string name;
+
+        public Figure(string name)
+        {
+            this.Name = name;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+
+            private set
+            {
+                if (value.Trim().Length != 1) 
+                {
+                    throw new ArgumentException("Invalid Figure Name");
+                }
+
+                this.name = value.Trim();
+            }
+        }
+
+    }
+}
