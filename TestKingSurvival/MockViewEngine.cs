b4c467821442b9d91b44fdb5f8edbb0aa72b20b1
Димо Петров <webdude@webdude.eu>
2014-07-20 13:47:34 +0300

    s

--- TestKingSurvival/MockViewEngine.cs
+++ TestKingSurvival/MockViewEngine.cs
@@ -11,37 +11,37 @@
 
         public void IllegalMove()
         {
-            throw new System.NotImplementedException();
+            
         }
 
         public void KingTurn()
         {
-            throw new System.NotImplementedException();
+
         }
 
         public void PawnsTurn()
         {
-            throw new System.NotImplementedException();
+     
         }
 
         public void InvalidFigure()
         {
-            throw new System.NotImplementedException();
+       
         }
 
         public void KingWin(int turns)
         {
-            throw new System.NotImplementedException();
+            
         }
 
         public void PawnsWin(int turns)
         {
-            throw new System.NotImplementedException();
+     
         }
 
         public void InvalidCommand()
         {
-            throw new System.NotImplementedException();
+
         }
     }
 }
